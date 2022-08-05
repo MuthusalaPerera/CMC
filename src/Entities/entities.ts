9d46f0673c20d64fdcd0389947ffdf04f4e4ae2e
unknown <gayanpoornima@gmail.com>
2022-08-05 10:57:51 +0530

    entity

---
+++ src/Entities/entities.ts
@@ -0,0 +1,7 @@
+import {User} from "../users/user.entity";
+import {ServiceCall} from "../service-calls/service-call.entity";
+import {SparePart} from "../spare-parts/spare-part.entity";
+
+const entities =[User,ServiceCall,SparePart];
+
+export default entities
