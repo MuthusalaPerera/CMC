e10577a19bd9064f9a0105c9b810749b2d86c3d3
Bamunusingha G.P <71841314+IT20011970@users.noreply.github.com>
2022-11-11 13:32:27 +0530

    new

--- src/Entities/entities.ts
+++ src/Entities/entities.ts
@@ -13,20 +13,19 @@ import {AssetMaster} from "../Item/AssetMaster"
 import {EquipmetCard} from "../Customer/EquipmetCard"
 import { OriginsDropDown } from "../IntialDB/Origin";
 import { ProblemTypesDropDown } from "src/IntialDB/ProblemType";
-
 import UserType from "../IntialDB/UserRolls";
 import { Solutions } from "src/ServiceCallOther/Solutions";
-
-
-const entities =[User,ServiceCall,SparePart,CustomerEntity,ItemEntity,ServiceTicketEntity ,UsersDropDown,userRolls,Login,ItemMasterEntity,AssetMaster,EquipmetCard,OriginsDropDown,ProblemTypesDropDown,UserType,Solutions];
-
 import {Expences} from "../ServiceCallOther/expences"
 import {AssertsReservation} from "../AssertsReservation/AssertsReservation";
 import {VehicleReservation} from "../Vehicle/VehicleReservation";
 import {Vehicle} from "../Vehicle/Vehicle";
 
+// const entities =[User,ServiceCall,SparePart,CustomerEntity,ItemEntity,ServiceTicketEntity ,UsersDropDown,userRolls,Login,ItemMasterEntity,AssetMaster,EquipmetCard,OriginsDropDown,ProblemTypesDropDown,UserType,Solutions];
 
-//const entities =[User,ServiceCall,SparePart,CustomerEntity,ItemEntity,ServiceTicketEntity ,UsersDropDown,userRolls,Login,ItemMasterEntity,AssetMaster,EquipmetCard,OriginsDropDown,ProblemTypesDropDown,Solutions,Expences,AssertsReservation,VehicleReservation,Vehicle];
+
+
+
+const entities =[User,ServiceCall,SparePart,CustomerEntity,ItemEntity,ServiceTicketEntity ,UsersDropDown,userRolls,Login,ItemMasterEntity,AssetMaster,EquipmetCard,OriginsDropDown,ProblemTypesDropDown,Solutions,Expences,AssertsReservation,VehicleReservation,Vehicle];
 
 
 
