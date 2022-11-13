e93098d19137fee06a37ae776ad7e6d11692dabb
Pawani Muthusala Perera <pawaniperera20@gmail.com>
2022-11-13 22:03:11 +0530

    new

---
+++ src/ResourceAllocation/resourceAllocation.entity.ts
@@ -0,0 +1,28 @@
+// import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+// import { CustomerEntity } from 'src/Customer/customer.entity';
+// import { ItemEntity } from 'src/Item/Item.entity';
+// import { ServiceTicketEntity } from 'src/service-calls/service-ticket.entity';
+// import {ServiceCall} from "../service-calls/service-call.entity";
+
+// @Entity()
+// export class SparePart {
+//  // [x: string]: string | number | Date | string[] | ObjectID | number[] | Date[] | ObjectID[] | FindConditions<SparePart>;
+//   @PrimaryGeneratedColumn()
+//   SPReqId: string;
+  
+//   @Column()
+//   Remark:string;
+  
+//   @Column()
+//   Content:string
+
+//   @Column()
+//   Secretary:string;
+
+//   @Column()
+//   ItemDescription:string;
+
+//   @ManyToOne(()=>ServiceTicketEntity,ServiceTicketEntity=>ServiceTicketEntity.sparePart,{onDelete:'CASCADE'})
+//   ServiceTicketEntity:ServiceTicketEntity
+  
+// }
---
+++ src/ResourceAllocation/resourceAllocation.module.ts
@@ -0,0 +1,19 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { CustomerEntity } from 'src/Customer/customer.entity';
+// import { ItemEntity } from 'src/Item/Item.entity';
+// import { ServiceTicketEntity } from 'src/service-calls/service-ticket.entity';
+// import { SparePart } from './spare-part.entity';
+// import { SparePartsController } from './spare-parts.controller';
+// import { SparePartsService } from './spare-parts.service';
+// import {ServiceCall} from "../service-calls/service-call.entity";
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([SparePart,ServiceCall,CustomerEntity,ItemEntity,ServiceTicketEntity])],
+//   controllers: [SparePartsController],
+//   providers: [{
+//     provide:'SpareParts_Service',
+//     useClass: SparePartsService
+//   }],
+// })
+// export class SparePartsModule {}
